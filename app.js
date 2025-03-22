d963b9f79e2b8612712960610954b4effe333d5a
Hadil Khelif <115453542+HADIL19@users.noreply.github.com>
2025-03-22 16:57:19 +0100

    app.js

--- app.js
+++ app.js
@@ -1,22 +1,51 @@
 const express = require('express') // Import express packages
 const app = express()// Create an express app
+app.use(express.json())
+const users = []
+
 //HTTP Methods
 //GET retrive data from server
-app.get('/', (req, res) => {
-    res.send('Welcome to home!')
+app.get('/', (request, response) => {
+    request.send('Welcome to home!')
 })
 
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
+app.get('/users', (request, response) => {
+    if(users.length==0){
+        response.status(404).send('no users found')
+        return
+    }
+    response.status(200).send(users)
 })
+
+//POST create data add 
+app.post('/users', (request, response) => {
+    const user = request.body ;
+    const findUser = users.find((x)=>x.id== user.id)
+    if (findUser){
+        response.status(400).send('user already exists');
+        return
+    }
+    users.push(user)
+    response.status(201).send('created!')
+})
+// PUT
+// DELETE - Remove data
+app.delete('/users/:id', (request, response) => {
+    const { id } = request.params;
+    const findUserIndex = users.findIndex((x) => x.id == id);
+    
+    if (findUserIndex == -1) {
+        response.status(400).send('User not found');
+        return;
+    }
+
+    users.splice(findUserIndex, 1);
+    response.status(200).send('User deleted successfully!');
+});
+
+// 
+
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
 })
-
-//POST create data
-app.post('/create', (req, res) => {
-    res.send('created data')
-})
-// PUT
-// DELETE
-//
